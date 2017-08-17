149c2222c383a01033b8e229c5a67631ed41f36f
github username <github email address>
2017-08-17 09:58:19 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,41 @@
+INSERT INTO series (title) VALUES ("GOT");
+INSERT INTO series (title) VALUES ("XMEN");
+
+INSERT INTO subgenres (name) VALUES ("LALA");
+INSERT INTO subgenres (name) VALUES ("NANA");
+
+INSERT INTO authors (name) VALUES ("George");
+INSERT INTO authors (name) VALUES ("Dr. Seuss");
+
+INSERT INTO books (title) VALUES ("1");
+INSERT INTO books (title) VALUES ("2");
+INSERT INTO books (title) VALUES ("3");
+INSERT INTO books (title) VALUES ("4");
+INSERT INTO books (title) VALUES ("5");
+INSERT INTO books (title) VALUES ("6");
+
+INSERT INTO characters (name) VALUES ("Bob");
+INSERT INTO characters (name) VALUES ("Bob");
+INSERT INTO characters (name) VALUES ("Bob");
+INSERT INTO characters (name) VALUES ("Bob");
+INSERT INTO characters (name) VALUES ("Bob");
+INSERT INTO characters (name) VALUES ("Bob");
+INSERT INTO characters (name) VALUES ("Bob");
+INSERT INTO characters (name) VALUES ("Bob");
+
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
+INSERT INTO character_books (character_id) VALUES ("1");
